d8033cfeb9ff795d893dc0d7b6dd2a2f981d1256
Brett Porter <brett@apache.org>
2009-08-31 21:18:17 +1000

    add tree formula

---
+++ Library/Formula/tree.rb
@@ -0,0 +1,17 @@
+require 'brewkit'
+
+class Tree <Formula
+  @url='ftp://mama.indstate.edu/linux/tree/tree-1.5.2.2.tgz'
+  @homepage='http://mama.indstate.edu/users/ice/tree/'
+  @md5='a7731a898e2c0d7e422a57a84ffbb06c'
+
+# def deps
+#   BinaryDep.new 'cmake'
+# end
+
+  def install
+    system "gcc #{ENV['CFLAGS']} -o tree tree.c strverscmp.c"
+    bin.install "tree"
+    man1.install "man/tree.1"
+  end
+end
